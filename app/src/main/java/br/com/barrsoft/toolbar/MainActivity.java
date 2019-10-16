eb98f4f142a9eb2ff865223280a5a2e048103927
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-16 00:45:59 -0300

    Adicionado ação ao menu

--- app/src/main/java/br/com/barrsoft/toolbar/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/toolbar/MainActivity.java
@@ -1,14 +1,63 @@
 package br.com.barrsoft.toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @Nullable
+    @BindView(R.id.menu_search) MenuItem menuItemSearch;
+    @Nullable
+    @BindView(R.id.menu_settings) MenuItem menuItemSettings;
+    @Nullable
+    @BindView(R.id.menu_about)MenuItem menuItemAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_toolbar , menu);
+        return true;
+        //return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_about:
+                Toast.makeText(this, "About", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
